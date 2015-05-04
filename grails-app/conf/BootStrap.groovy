8a02ace94b6ec3456ce37c8cd4d10717b963b4a9
Angel <iscjangelhg@gmail.com>
2015-05-04 02:40:31 -0500

    Se agrega registro de usuario administrador al archivo bootstrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,19 @@
+import security.Role
+import security.User
+import security.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+
+
+        User user
+        Role role
+        UserRole userRole
+
+        role = new Role(authority: 'ROLE_ADMIN', name: 'ADMINISTRADOR').save(flush: true)
+        user = new User(username: 'admin', password: 'admin', passwordExpired: false, accountExpired: false, accountLocked: false, enabled: true).save(true)
+        userRole = new UserRole(user:user,role:role).save(flush: true,insert: true)
     }
     def destroy = {
     }
