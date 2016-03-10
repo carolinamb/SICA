906daf2728773ae613e1c33c06bb04e2c364c1e9
Carolina Martinez <caromb64@gmail.com>
2016-03-09 22:26:48 -0600

    se agrgan campos a operaciones diarias

--- grails-app/domain/operacionDiaria/Movimiento.groovy
+++ grails-app/domain/operacionDiaria/Movimiento.groovy
@@ -13,6 +13,13 @@ class Movimiento {
     static hasMany = [armasOcupadas:Arma]
     Boolean estatus
 
+    String cartuchosEntregados
+    String cartuchosRecibidos
+    Long cargadoresEntregados
+    Long cargadoresRecibidos
+    Date fecha
+    String observaciones
+
 
     Date fechaEntrega
     Date fechaRecepcion
@@ -22,6 +29,12 @@ class Movimiento {
         armasOcupadas nullable: true
         armero nullable: false
         turno nullable: false
+        cartuchosEntregados nullable: true
+        cartuchosRecibidos nullable: true
+        cargadoresEntregados nullable: true
+        cargadoresRecibidos nullable: true
+        fecha nullable: true
+        observaciones nullable: true
         fechaEntrega nullable: true
         fechaRecepcion nullable: true
         estatus nullable: true
