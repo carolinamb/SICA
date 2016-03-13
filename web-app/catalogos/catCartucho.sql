9b195eb3d36de6a18a9dfbc113dc7074cec6dd44
angelHergon <iscjangelhg@gmail.com>
2016-03-13 17:20:36 -0600

    Se eliminan vistas, controlladores, dominios, catalogos de cargadores y cartuchos

--- web-app/catalogos/catCartucho.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO cartucho (id,version,calibre,estatus,cantidad) VALUES (1,0,'CALIBRE1',true,1);
-INSERT INTO cartucho (id,version,calibre,estatus,cantidad) VALUES (2,0,'CALIBRE2',true,2);
-INSERT INTO cartucho (id,version,calibre,estatus,cantidad) VALUES (3,0,'CALIBRE3',true,1);
-INSERT INTO cartucho (id,version,calibre,estatus,cantidad) VALUES (4,0,'CALIBRE4',true,1);
-INSERT INTO cartucho (id,version,calibre,estatus,cantidad) VALUES (5,0,'CALIBRE5',true,2);
-INSERT INTO cartucho (id,version,calibre,estatus,cantidad) VALUES (6,0,'CALIBRE6',true,1);
-INSERT INTO cartucho (id,version,calibre,estatus,cantidad) VALUES (7,0,'CALIBRE7',true,2);
-INSERT INTO cartucho (id,version,calibre,estatus,cantidad) VALUES (8,0,'CALIBRE8',true,2);
